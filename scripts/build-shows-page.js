96fa5acdbef8f23a3169111fc23876c71accaee5
Chelsea Quindipan <quindipc@gmail.com>
2023-06-16 21:29:35 -0400

    Update the shows js and change the toggle behavior

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -5,33 +5,28 @@ const API_KEY = "34dbda0f-4c31-43b8-90aa-1eda3e3f88a4";
 ///////////////// SHOWS /////////////////
 // Get /showdates
 function fetchShows() {
-    axios
-        .get(`${BASE_URL}/showdates?api_key=${API_KEY}`)
-        .then(
-            (response) => {
-                console.log(response);
-                const showsData = response.data;
+  axios
+    .get(`${BASE_URL}/showdates?api_key=${API_KEY}`)
+    .then((response) => {
+      console.log(response);
+      const showsData = response.data;
 
-                // Loop over the shows data and create show items
-                showsData.forEach((show) => {
- 
-                    // Date into readable format
-                    const date = new Date(show.date);
-                    const formattedDate = date.toDateString();
+      // Loop over the shows data and create show items
+      showsData.forEach((show) => {
+        // Date into readable format
+        const date = new Date(show.date);
+        const formattedDate = date.toDateString();
 
-                    createShowItem({
-                        date: formattedDate,
-                        venue: show.place,
-                        location: show.location,
-                    });
-                });
-            })
-        .catch(
-            (error) => {
-                console.error(error);
-            }
-        );
-
+        createShowItem({
+          date: formattedDate,
+          venue: show.place,
+          location: show.location,
+        });
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 fetchShows();
 
@@ -81,67 +76,65 @@ showsListContainer.appendChild(tabletHeader);
 
 // Creates a show item
 function createShowItem(show) {
-    const showsItemList = document.createElement("div");
-    showsItemList.classList.add("shows__list-item");
+  const showsItemList = document.createElement("div");
+  showsItemList.classList.add("shows__list-item");
 
-    // Add click event to toggle background color
-    showsItemList.addEventListener("click", () => {
-        toggleBackgroundColor(showsItemList);
-    });
+  // Add click event to toggle background color
+  showsItemList.addEventListener("click", () => {
+    toggleBackgroundColor(showsItemList);
+  });
 
-    // Appends the date, venue, location, and button
-    appendSubheader(showsItemList, "Date", show.date);
-    appendSubheader(showsItemList, "Venue", show.venue);
-    appendSubheader(showsItemList, "Location", show.location);
+  // Appends the date, venue, location, and button
+  appendSubheader(showsItemList, "Date", show.date);
+  appendSubheader(showsItemList, "Venue", show.venue);
+  appendSubheader(showsItemList, "Location", show.location);
 
-    // Button Container
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("shows__button-container");
+  // Button Container
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("shows__button-container");
 
-    // Button element
-    const buyTicketsButton = document.createElement("button");
-    buyTicketsButton.classList.add("shows__button");
-    buyTicketsButton.textContent = "Buy Tickets";
+  // Button element
+  const buyTicketsButton = document.createElement("button");
+  buyTicketsButton.classList.add("shows__button");
+  buyTicketsButton.textContent = "Buy Tickets";
 
-    // Append the button to the button container
-    buttonContainer.appendChild(buyTicketsButton);
+  // Append the button to the button container
+  buttonContainer.appendChild(buyTicketsButton);
 
-    // Append the button container to the show item
-    showsItemList.appendChild(buttonContainer);
+  // Append the button container to the show item
+  showsItemList.appendChild(buttonContainer);
 
-
-    // Append to the shows container
-    showsListContainer.appendChild(showsItemList);
+  // Append to the shows container
+  showsListContainer.appendChild(showsItemList);
 }
 
 // Function to add the shows subheader and shows info to the parent
 function appendSubheader(parent, label, text) {
-    const subheader = document.createElement("h3");
-    subheader.classList.add("shows__subheader");
-    subheader.textContent = label;
+  const subheader = document.createElement("h3");
+  subheader.classList.add("shows__subheader");
+  subheader.textContent = label;
 
-    const info = document.createElement("p");
-    info.classList.add("shows__info");
-    info.textContent = text;
+  const info = document.createElement("p");
+  info.classList.add("shows__info");
+  info.textContent = text;
 
-    parent.appendChild(subheader);
-    parent.appendChild(info);
+  parent.appendChild(subheader);
+  parent.appendChild(info);
 }
 
 // Function to append a divider line in the shows container
 function appendDividerLine(parent) {
-    const dividerLine = document.createElement("hr");
-    dividerLine.classList.add("shows__divider");
-    parent.appendChild(dividerLine);
+  const dividerLine = document.createElement("hr");
+  dividerLine.classList.add("shows__divider");
+  parent.appendChild(dividerLine);
 }
 
 ///////////////// TOGGLE /////////////////
 // Function to toggle background color
 function toggleBackgroundColor(element) {
-    const backgroundColor = element.style.backgroundColor;
-    if (backgroundColor && backgroundColor === "rgb(225, 225, 225)") {
-        element.style.backgroundColor = "";
-    } else {
-        element.style.backgroundColor = "#e1e1e1";
-    }
+  const shows = document.getElementsByClassName("shows__list-item");
+  for (let i = 0; i < shows.length; i++) {
+    shows[i].classList.remove("selected");
+  }
+  element.classList.add("selected");
 }
